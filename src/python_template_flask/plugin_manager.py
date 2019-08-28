1b5e8a32113c988149bf9a384cc80d814cc25cf9
Sijis Aviles <sijis.aviles@gmail.com>
2019-08-27 20:51:01 -0500

    refactor: PluginManager should not require a plugin name
    The plugin name is only necessary when we are attempting to load a
    plugin. Moving the plugin name requirement to load() is more appropriate.

--- src/python_template_flask/plugin_manager.py
+++ src/python_template_flask/plugin_manager.py
@@ -9,16 +9,13 @@ class PluginManager:
 
     Args:
         resource (str): Path of plugin directory.
-        name (str): The name of the plugin.
     """
-    def __init__(self, resource, name):
+    def __init__(self, resource):
         path = pathlib.Path(__file__).parent.resolve()
         path = path / resource
 
         all_paths = [str(path)]
-
         self.paths = all_paths
-        self.name = name
 
         plugin_base = PluginBase(package='python_template_flask.plugins')
         self.plugin_source = plugin_base.make_plugin_source(searchpath=self.paths, persist=True)
@@ -28,16 +25,25 @@ class PluginManager:
         for plugin in self.plugin_source.list_plugins():
             yield plugin
 
-    def load(self):
-        """Load the plugin."""
-        loaded_plugin = self.plugin_source.load_plugin(self.name)
+    def load(self, name):
+        """Load the plugin.
+
+        Args:
+            name (str): The name of the plugin.
+        """
+        loaded_plugin = self.plugin_source.load_plugin(name)
         return loaded_plugin
 
 
 def get_plugin(plugin_dir, plugin_name):
-    """Wrapper around PluginManager"""
-    manager = PluginManager(plugin_dir, plugin_name)
-    plugin = manager.load()
+    """Wrapper around PluginManager
+
+    Args:
+        plugin_dir (str): Path of plugin directory.
+        name (str): The name of the plugin.
+    """
+    manager = PluginManager(plugin_dir)
+    plugin = manager.load(plugin_name)
     return plugin
 
 
@@ -50,7 +56,7 @@ def get_plugins(plugin_dir, prefix=None):
     """
     if not prefix:
         prefix = ''
-    manager = PluginManager(plugin_dir, '')
+    manager = PluginManager(plugin_dir)
     available_plugins = manager.plugins()
     for plugin in available_plugins:
         if plugin.startswith(prefix):
